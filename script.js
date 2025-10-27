fa7fb13513920d4015247bb9037b9b2cef61640e
Weryyxx <bogdanbebykh@gmail.com>
2025-10-27 22:10:41 +0200

    Created script.js, added mobile burger menu toggle functionality

---
+++ script.js
@@ -0,0 +1,5 @@
+burder_menu = document.querySelector(".burger_menu_mobile");
+
+burder_menu.addEventListener("click", function() {
+  document.querySelector(".sidebar_mobile").classList.toggle("sidebar_mobile_active");
+});
