235d230bce412bd2d7a3475d2a9b2c615c4e2d00
Artem222cmd <timaivanis139@gmail.com>
2025-10-28 19:03:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 burder_menu = document.querySelector(".burger_menu_mobile");
 
+
 burder_menu.addEventListener("click", function() {
   document.querySelector(".sidebar_mobile").classList.toggle("sidebar_mobile_active");
+  
+
+  if (document.querySelector(".mobile_search_container").classList.contains("mobile_search_container_active")) {
+      document.querySelector(".mobile_search_container").classList.remove("mobile_search_container_active")
+  }
+});
+
+search_icon_mobile = document.querySelector(".search_icon_mobile");
+
+
+search_icon_mobile.addEventListener("click", function() {
+  
+  document.querySelector(".mobile_search_container").classList.toggle("mobile_search_container_active");
+  
+  if (document.querySelector(".sidebar_mobile").classList.contains("sidebar_mobile_active")) {
+      document.querySelector(".sidebar_mobile").classList.remove("sidebar_mobile_active")
+  }
+
 });
